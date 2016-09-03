381e0bb54e15898f6fe20c49a38b05d805182b06
Pete Schuster <pas5027@gmail.com>
2016-09-03 15:23:05 -0400

    adds autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,42 @@ var gulp = require('gulp'),
     path = require('path'),
     sass = require('gulp-sass'),
     minifycss = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
     livereload = require('gulp-livereload'),
     uglify = require('gulp-uglify');
 
 // styles
 
 gulp.task('styles:dev', function () {
-  return gulp.src('sass/main.scss')
-  .pipe(sass()).on('error', sass.logError)
-  .pipe(concat('main.css'))
-  .pipe(gulp.dest('css'))
-  .pipe(livereload());
+    return gulp.src('sass/main.scss')
+        .pipe(sass()).on('error', sass.logError)
+        .pipe(concat('main.css'))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('css'))
+        .pipe(livereload());
 });
 
 gulp.task('styles:prod', function () {
-  return gulp.src('sass/main.scss')
-  .pipe(sass()).on('error', sass.logError)
-  .pipe(concat('main.min.css'))
-  .pipe(minifycss())
-  .pipe(gulp.dest('css'));
+    return gulp.src('sass/main.scss')
+        .pipe(sass()).on('error', sass.logError)
+        .pipe(concat('main.min.css'))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('css'));
 });
 
 gulp.task('styles:ie', function () {
-  return gulp.src('sass/ie.scss')
-  .pipe(sass()).on('error', sass.logError)
-  .pipe(concat('ie.min.css'))
-  .pipe(minifycss())
-  .pipe(gulp.dest('css'));
+    return gulp.src('sass/ie.scss')
+        .pipe(sass()).on('error', sass.logError)
+        .pipe(concat('ie.min.css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('css'));
 });
 
 gulp.task('styles', ['styles:dev', 'styles:prod', 'styles:ie']);
@@ -39,44 +48,47 @@ gulp.task('styles', ['styles:dev', 'styles:prod', 'styles:ie']);
 // scripts
 
 gulp.task('scripts:top:dev', function () {
-  return gulp.src([
-    'javascripts/vendor/modernizr.js',
-    'javascripts/vendor/plugins.js'
-  ])
-  .pipe(concat('top.js'))
-  .pipe(gulp.dest('js'));
+    return gulp.src([
+        'javascripts/vendor/modernizr.js',
+        'javascripts/vendor/plugins.js'
+    ])
+        .pipe(concat('top.js'))
+        .pipe(gulp.dest('js'));
 });
 
 gulp.task('scripts:top:prod', function () {
-  return gulp.src([
-    'javascripts/vendor/modernizr.js',
-    'javascripts/vendor/plugins.js'
-  ])
-  .pipe(concat('top.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('js'));
+    return gulp.src([
+        'javascripts/vendor/modernizr.js',
+        'javascripts/vendor/plugins.js'
+    ])
+        .pipe(concat('top.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('js'));
 });
 
 gulp.task('scripts:main:dev', function () {
-  return gulp.src([
-    'javascripts/main.js',
-  ])
-  .pipe(concat('main.js'))
-  .pipe(gulp.dest('js'));
+    return gulp.src([
+        'javascripts/vendor/slick.js',
+        'javascripts/main.js'
+    ])
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('js'));
 });
 
 gulp.task('scripts:main:prod', function () {
-  return gulp.src([
-    'javascripts/main.js',
-  ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('js'));
+    return gulp.src([
+        'javascripts/vendor/slick.js',
+        'javascripts/main.js'
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('js'));
 });
 
 gulp.task('scripts', ['scripts:top:dev', 'scripts:top:prod', 'scripts:main:dev', 'scripts:main:prod']);
 
 gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch('sass/**/*.scss', ['styles:dev']);
+    livereload.listen();
+    gulp.watch('sass/**/*.scss', ['styles']);
+    gulp.watch('javascripts/**/*.js', ['scripts']);
 });
